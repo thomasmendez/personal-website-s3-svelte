4257f03063d2c47c6a24561c4868c12d1c3e216f
thomasmendez <thomasmendez01@gmail.com>
2023-10-29 02:33:05 -0500

    Added daisyui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
     import "../app.css"
 </script>
 
-<div class="bg-gray-900 text-white container mx-auto my-6 max-w-lg">
+<!-- <div class="bg-gray-900 text-white container mx-auto my-6 max-w-lg">
     <slot></slot>
-</div>
+</div> -->
+
+<slot />
