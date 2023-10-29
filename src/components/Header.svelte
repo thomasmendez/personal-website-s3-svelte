704fea5d6b785cbf38d181f47224d761fe97262a
thomasmendez <thomasmendez01@gmail.com>
2023-10-29 16:11:18 -0500

    Simple navbar

---
+++ src/components/Header.svelte
@@ -0,0 +1,25 @@
+<div class="navbar bg-zinc-500 text-neutral-content">
+    <div class="flex-1">
+      <a class="btn btn-ghost normal-case text-xl">Thomas A. Mendez</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>About</a></li>
+        <li><a>Work</a></li>
+        <li><a>Skills &amp; Tools</a></li>
+        <li>
+          <details>
+            <summary>
+              Projects
+            </summary>
+            <ul class="text-zinc-500">
+              <li><a>Software Engineering</a></li>
+              <li><a>VR / AR</a></li>
+            </ul>
+          </details>
+        </li>
+        <li><a>Resume</a></li>
+        <li><a>Storybook</a></li>
+      </ul>
+    </div>
+  </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
     import "../app.css"
+    import Header from '../components/Header.svelte'
 </script>
 
 <!-- <div class="bg-gray-900 text-white container mx-auto my-6 max-w-lg">
     <slot></slot>
 </div> -->
 
-<slot />
+<!-- <slot /> -->
+
+<Header />
